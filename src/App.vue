5fb3a17f9c4efd4274919c151d1d4253edd61af4
magalyyanezg <a3521110208@alumno.uttehuacan.edu.mx>
2022-09-29 23:25:35 -0500

    Quinta actualizacion

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,35 @@
     <h2> APW</h2>
     <h3>Grupo 4A</h3>
     <h1>Directivas en VUE.js</h1>
+    <h1>v-html</h1>
+    <div v-html="html"></div>
     <hr />
-    <h2>v-text</h2>
-    <span v-text="'Hola desde la directiva v-text'"> </span>
-    <br>
-    <span>Hola desde la directiva v-text</span>
-    <br>
-    <span v-text="titulo"> </span>
-    <br>
-    Esta es una transmutación de texto ({{ titulo }}), con una variable prop <br>
-    
+    <h1>v-show</h1>
+    <h3>Tu edad es:  {{ edad }}</h3>
+    <h2 v-if="edad >= 18 && edad > 0 && edad < 100">Mayor de edad</h2>
+    <h2 v-if="edad < 18 && edad > 0 && edad < 100">Menor de edad</h2>
+    <h2 v-else>Edad invalida</h2>
+    <button @click="inc()">incrementar</button>
+    <button @click="dis()">disminuir</button>
+
+    <hr />
+    <h1>v-for</h1>
   </div>
 </template>
 
 <script>
   export default {
     data:() => ( {
-      titulo:"Hola desde VUE.js",
+      titulo:"Hola desde VUE.js 3",
+      html: "<h3> Soy un H3 </h3>",
+      edad: 0,
 
     }),
+    methods: {
+      inc() {
+        this.edad++;
+      },
+    },
   };
 </script>
 
