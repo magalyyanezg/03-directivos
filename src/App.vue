bd588771b8111fc9852f7e6d5744d03a2f6c53de
magalyyanezg <a3521110208@alumno.uttehuacan.edu.mx>
2022-10-02 20:01:30 -0500

    Sexta actualización de proyecto 02/10/22

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,38 @@
 
     <hr />
     <h1>v-for</h1>
+    <ul>
+      <li>{{usuarios[0] }}</li>
+      <li>{{usuarios[1] }}</li>
+      <li>{{usuarios[2] }}</li>
+      <li>{{usuarios[3] }}</li>
+    </ul>
+    <br>
+    <ul>
+      <li v-for="usuario in usuarios"> {{ usuario }}</li>
+    </ul>
+    <br>
+    <ul>
+      <li v-for="(usuario, index) in usuarios"> {{  index }}: {{ usuario }}</li>
+    </ul>
+    <br>
+    <ul>
+      <li v-for="(valor, clave) in uusuarios" :key="clave"> {{  clave }}: {{ valor }}</li>
+    </ul>
+
+    <hr />
+    <h1>v-bind</h1>
+    <img v-bin:src="url" alt="No hay internet">
   </div>
+
+    <hr />
+    <h1>v-on</h1>
+    <div @dblclick="click" class="contador">
+      Clicks: {{clicks}}
+    </div>
+
+
+
 </template>
 
 <script>
@@ -27,11 +58,20 @@
       titulo:"Hola desde VUE.js 3",
       html: "<h3> Soy un H3 </h3>",
       edad: 0,
-
+      usuarios: ["Melisa", "Fernando", "Raul", "Jose"],
+      uusuarios: {nombre: "Yirsis"},
+      clicks:0,
+      url:"https://cdn.svgporn.com/logos/descript-icon.svg",
     }),
     methods: {
       inc() {
         this.edad++;
+      },
+      dis () {
+        this.edad--;
+      },
+      click() {
+        this.clicks++;
       },
     },
   };
@@ -41,5 +81,14 @@
   div {
     text-align: center;
     margin: 0px 2rem;
+  }
+  h2 {
+    color: tomato;
+  }
+  .contador {
+    background: pink;
+    color: snow;
+    font-weight: bold;
+    height: 400px;
   }
 </style>
